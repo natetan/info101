77c4fed937cd975d1aeafcc4d7506be55ee4a9f0
yulongtan <swifties@uw.edu>
2015-10-22 12:10:23 -0700

    set up the variables for the links

---
+++ obama.js
@@ -0,0 +1,5 @@
+var left1 = document.getElementById("left1");
+var left2 = document.getElementById("left2");
+var left3 = document.getElementById("left3");
+var left4 = document.getElementById("left4");
+var left5 = document.getElementById("left5");
